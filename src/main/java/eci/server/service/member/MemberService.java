fef91abbe513422fe7ecedcf00950668e5816e55
myway00 <junior0614@naver.com>
2022-03-22 14:07:50 +0900

    #2 add : member get,delete 추가

---
+++ src/main/java/eci/server/service/member/MemberService.java
@@ -0,0 +1,30 @@
+package eci.server.service.member;
+
+import eci.server.dto.member.MemberDto;
+import eci.server.exception.member.MemberNotFoundException;
+import eci.server.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberDto read(Long id) {
+        return MemberDto.toDto(memberRepository.findById(id).orElseThrow(MemberNotFoundException::new));
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if(notExistsMember(id)) throw new MemberNotFoundException();
+        memberRepository.deleteById(id);
+    }
+
+    private boolean notExistsMember(Long id) {
+        return !memberRepository.existsById(id);
+    }
+
+}
