a3aab85d13a8dfc86103906d418b296f524ed590
myway00 <junior0614@naver.com>
2022-04-15 18:42:46 +0900

    #9 fix : FLOW

--- src/main/java/eci/server/ItemModule/dto/route/RouteUpdateRequest.java
+++ src/main/java/eci/server/ItemModule/dto/route/RouteUpdateRequest.java
@@ -26,7 +26,7 @@ public class RouteUpdateRequest {
     private String type;
     @Null
     private String workflow;
-    @Null
+
     private String workflowPhase;
     @Null//develop 이랑 release 중 하나
     private String lifecycleStatus;
