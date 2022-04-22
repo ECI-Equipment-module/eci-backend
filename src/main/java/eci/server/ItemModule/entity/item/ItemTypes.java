323f6394115f92a8c16a5d8430435dd7266451c4
myway00 <junior0614@naver.com>
2022-04-22 17:12:54 +0900

    #6 refactor : item - types 를 다대다로 설정 및 itemTypes다대다 테이블 생성

--- src/main/java/eci/server/ItemModule/entity/item/ItemTypes.java
+++ src/main/java/eci/server/ItemModule/entity/item/ItemTypes.java
@@ -1,41 +1,26 @@
 package eci.server.ItemModule.entity.item;
 
-import eci.server.ItemModule.entity.route.Route;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Slf4j
+@AllArgsConstructor
+@EqualsAndHashCode
+@IdClass(ItemTypesId.class)
 public class ItemTypes {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="SEQUENCE1")
-    @SequenceGenerator(name="SEQUENCE1", sequenceName="SEQUENCE1", allocationSize=1)
-    private Long id;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Item item;
 
+    @Id
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private ItemTypes parent;
-
-    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
-    private List<ItemTypes> children = new ArrayList<>();
+    @JoinColumn(name = "types_id")
+    private Types types;
 
 
 }
---
+++ src/main/java/eci/server/ItemModule/entity/item/Types.java
@@ -0,0 +1,36 @@
+package eci.server.ItemModule.entity.item;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Slf4j
+public class Types {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="SEQUENCE1")
+    @SequenceGenerator(name="SEQUENCE1", sequenceName="SEQUENCE1", allocationSize=1)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Types parent;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+    private List<Types> children = new ArrayList<>();
+
+
+}
