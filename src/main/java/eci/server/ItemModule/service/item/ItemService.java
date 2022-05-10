769a871c0f2d8243560a7204ec068735b7689a67
myway00 <junior0614@naver.com>
2022-05-11 02:05:43 +0900

    #6 fix : 에러유발메시지 삭제

--- src/main/java/eci/server/ItemModule/service/item/ItemService.java
+++ src/main/java/eci/server/ItemModule/service/item/ItemService.java
@@ -107,7 +107,6 @@ public class ItemService {
 
     @Transactional
     public ItemCreateResponse create(ItemCreateRequest req) {
-        //req.getAttachments().get(0).getContentType();
 
         Item item = itemRepository.save(
                 ItemCreateRequest.toEntity(
