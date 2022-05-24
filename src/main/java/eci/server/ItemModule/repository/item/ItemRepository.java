832f6acfb2b690b22086bd32a2caa6f290ae0782
myway00 <junior0614@naver.com>
2022-05-24 17:45:08 +0900

    #99 fix : 프로젝트 paging 결과 수정

--- src/main/java/eci/server/ItemModule/repository/item/ItemRepository.java
+++ src/main/java/eci/server/ItemModule/repository/item/ItemRepository.java
@@ -1,11 +1,7 @@
 package eci.server.ItemModule.repository.item;
 
-import eci.server.ItemModule.dto.item.ItemProjectCreateReadCondition;
-import eci.server.ItemModule.dto.item.ItemProjectCreateResponse;
 import eci.server.ItemModule.entity.item.Item;
 import eci.server.ItemModule.entity.member.Member;
-import eci.server.ProjectModule.dto.carType.CarTypeReadCondition;
-import eci.server.ProjectModule.dto.carType.CarTypeReadResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
