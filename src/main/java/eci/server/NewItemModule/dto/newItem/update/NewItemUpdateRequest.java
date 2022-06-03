bdf035341d8a3bbc059855d1971c0591e2ae6466
myway00 <junior0614@naver.com>
2022-06-03 16:22:16 +0900

    #24 fix : create request 주석 취소, update를 주석 처리

--- src/main/java/eci/server/NewItemModule/dto/newItem/update/NewItemUpdateRequest.java
+++ src/main/java/eci/server/NewItemModule/dto/newItem/update/NewItemUpdateRequest.java
@@ -1,12 +1,12 @@
-package eci.server.NewItemModule.dto.newItem.update;
-
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class NewItemUpdateRequest {
-}
+//package eci.server.NewItemModule.dto.newItem.update;
+//
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class NewItemUpdateRequest {
+//}
