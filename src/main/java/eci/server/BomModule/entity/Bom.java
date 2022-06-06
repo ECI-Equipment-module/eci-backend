3ff5463182bdaf23af681b0a5974602ef7fcc3b4
myway00 <junior0614@naver.com>
2022-06-06 12:09:07 +0900

    #24 fix : generation fixed

--- src/main/java/eci/server/BomModule/entity/Bom.java
+++ src/main/java/eci/server/BomModule/entity/Bom.java
@@ -19,9 +19,9 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Bom extends EntityDate {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE2")
-//    @SequenceGenerator(name="SEQUENCE2", sequenceName="SEQUENCE2", allocationSize=1)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE2")
+    @SequenceGenerator(name="SEQUENCE2", sequenceName="SEQUENCE2", allocationSize=1)
 
     private Long id;
 
