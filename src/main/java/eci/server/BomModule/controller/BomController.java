90e2d840c8689eeeae5d8be0bffde9c6f87e0d1e
myway00 <junior0614@naver.com>
2022-06-07 17:10:22 +0900

    #123 fix : controller에 service 어노테이션 붙인 것, restcontroller로 수정

--- src/main/java/eci/server/BomModule/controller/BomController.java
+++ src/main/java/eci/server/BomModule/controller/BomController.java
@@ -5,20 +5,26 @@ import eci.server.BomModule.service.BomService;
 import eci.server.ItemModule.dto.response.Response;
 import eci.server.aop.AssignMemberId;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Service
-@Transactional(readOnly = true)
+@RestController
 @RequiredArgsConstructor
+@Slf4j
+@CrossOrigin(origins = "https://localhost:3000")
 public class BomController {
+
     private final BomService bomService;
 
+    @CrossOrigin(origins = "https://localhost:3000")
     @PostMapping("/preliminary")
     @ResponseStatus(HttpStatus.CREATED)
     @AssignMemberId
