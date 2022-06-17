5d57539d7966cfb183119cc580c5b9f1dedb3bd6
myway00 <junior0614@naver.com>
2022-06-18 01:44:09 +0900

    #24 fix : 커스텀 메이커 생성자 추가

--- src/main/java/eci/server/NewItemModule/dto/maker/CustomMakerResponse.java
+++ src/main/java/eci/server/NewItemModule/dto/maker/CustomMakerResponse.java
@@ -1,5 +1,8 @@
 package eci.server.NewItemModule.dto.maker;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class CustomMakerResponse {
     private Long id;
     private String code;
