c7e1c7a864ff1fbd2747e4453332daea39250a35
myway00 <junior0614@naver.com>
2022-06-30 16:52:05 +0900

    #152 add :이미 A(부모)-B(자식)으로 선언된 관계가 B(부모)-A(자식)으로 선언될시 예외 처리

--- src/main/java/eci/server/BomModule/service/DevelopmentBomService.java
+++ src/main/java/eci/server/BomModule/service/DevelopmentBomService.java
@@ -4,6 +4,7 @@ import eci.server.BomModule.dto.dev.DevelopmentRequestDto;
 import eci.server.BomModule.entity.DevelopmentBom;
 import eci.server.BomModule.exception.AddedDevBomNotPossible;
 import eci.server.BomModule.exception.DevelopmentBomNotFoundException;
+import eci.server.BomModule.exception.InadequateRelationException;
 import eci.server.BomModule.repository.DevelopmentBomRepository;
 import eci.server.ItemModule.exception.item.ItemNotFoundException;
 import eci.server.ItemModule.repository.newRoute.RouteOrderingRepository;
@@ -15,6 +16,7 @@ import eci.server.NewItemModule.repository.item.NewItemRepository;
 import eci.server.NewItemModule.service.item.NewItemService;
 import eci.server.ProjectModule.dto.project.ProjectCreateUpdateResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -83,14 +85,21 @@ public class DevelopmentBomService {
             }
 
             List<Long> newIdList = new ArrayList<>();
+            List<Long> reversedIdList = new ArrayList<>(); //0630 - 무한 재귀 방지 체크
             int s = 0;
 
             while (s < req.getChildId().size()) {
 
                 Long newId = Long.parseLong(req.getParentId().get(s).toString() +
                         req.getChildId().get(s).toString());
-                System.out.println(newId);
+
                 newIdList.add(newId);
+
+                Long reversedNewId = Long.parseLong(req.getChildId().get(s).toString() +
+                        req.getParentId().get(s).toString()); //0630 - 무한 재귀 방지 체크
+
+                reversedIdList.add(reversedNewId); //0630 - 무한 재귀 방지 체크
+
                 s += 1;
 
             }
@@ -128,9 +137,26 @@ public class DevelopmentBomService {
                         ItemNotFoundException::new
                 );
 
-                if (tempNewItemParentChildrenRepository.findById(
+                if (
+                    //안 만들어졌던 관계일 경우에만 만들어주기
+                        tempNewItemParentChildrenRepository.findById(
                         newIdList.get(i)
-                ).isEmpty()) { //안 만들어졌던 관계일 경우에만 만들어주기
+                ).isEmpty()
+
+                ) {
+
+                    if(tempNewItemParentChildrenRepository.findById(
+                            reversedIdList.get(i)
+                    ).isPresent()){
+                        // (+) 06-30 : PARENT-CHILD 관계로 이미 존재하는데
+                        // CHILD가 PARENT 되고 PARENT가 이의 CHILD가 되는 관계는 ABSURD
+                        // 즉 101-102 이렇게 부모 자식인데, 102-101 이렇게 부모 자식 되면 안됨 => 막아줄 것임
+
+                        throw new InadequateRelationException();
+                    }
+
+
+
                     tempNewItemParentChildrenRepository.save(
                             new TempNewItemParentChildren(
                                     newIdList.get(i),
