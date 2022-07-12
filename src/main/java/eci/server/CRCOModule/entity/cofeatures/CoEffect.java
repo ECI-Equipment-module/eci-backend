c5d461b90a93adc4ec6cbda38b8b2be95d491a15
myway00 <junior0614@naver.com>
2022-07-12 17:01:11 +0900

    #170 add : revise cr, co critical : item revise new 로 새로 생성할 때 target route id, revision이 set이 안되고 있었다.

--- src/main/java/eci/server/CRCOModule/entity/cofeatures/CoEffect.java
+++ src/main/java/eci/server/CRCOModule/entity/cofeatures/CoEffect.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CoEffect{
     @Id
-    //    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE2")
     @SequenceGenerator(name="SEQUENCE2", sequenceName="SEQUENCE2", allocationSize=1)
 
--- src/main/java/eci/server/CRCOModule/entity/cofeatures/CoStage.java
+++ src/main/java/eci/server/CRCOModule/entity/cofeatures/CoStage.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CoStage {
     @Id
-    //    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQUENCE2")
     @SequenceGenerator(name="SEQUENCE2", sequenceName="SEQUENCE2", allocationSize=1)
 
