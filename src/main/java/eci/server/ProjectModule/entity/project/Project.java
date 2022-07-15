99a6bc2c2b2199bbbbe2bca0971e7ed8ca55e8ca
myway00 <junior0614@naver.com>
2022-07-15 10:25:05 +0900

    #167 add : update 2 메소드 추가

--- src/main/java/eci/server/ProjectModule/entity/project/Project.java
+++ src/main/java/eci/server/ProjectModule/entity/project/Project.java
@@ -778,4 +778,18 @@ public class Project extends EntityDate {
                 this.id
         );
     }
+
+    public NewItemCreateResponse updateNewItem(
+            Long NewItemId,
+            NewItemRepository newItemRepository
+    ){
+        this.newItem =
+                newItemRepository.findById(NewItemId)
+                        .orElseThrow(ItemNotFoundException::new);
+
+        return new NewItemCreateResponse(newItem.getId());
+
+    }
+
+
 }
